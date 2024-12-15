36e8a2aafc336c6e88d1656cdd2d3a2058b874e2
Fastestgod <124069731+Fastestgod@users.noreply.github.com>
2024-12-14 20:14:18 -0500

    push

--- todolist-backend/index.js
+++ todolist-backend/index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
     res.json("Hello");
 })
 // MongoDB connection
-const mongoURI = process.env.MONGO_URL;
+const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => {
